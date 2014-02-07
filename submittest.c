a62fb415f97288c099d51fd0cce64c0ce63701f7
Rob Clark <robdclark@gmail.com>
2014-02-07 08:15:01 -0500

    submittest: add bogus submit test
    Add test to check for a bo appearing multiple times in a submit.

--- submittest.c
+++ submittest.c
@@ -40,6 +40,12 @@
 #include <freedreno_drmif.h>
 #include <freedreno_ringbuffer.h>
 
+#define __user
+#define U642VOID(x) ((void *)(unsigned long)(x))
+#define VOID2U64(x) ((uint64_t)(unsigned long)(x))
+
+#include "msm_drm.h"
+
 #include "util.h"
 #include "ring.h"
 #include "adreno_common.xml.h"
@@ -48,6 +54,8 @@
 /* simple test for submit ioctl handling, in particular range checking
  * on cmdstream buffer
  */
+
+static int test_invalid_submit(int fd, struct fd_device *dev);
 
 int main(int argc, char *argv[])
 {
@@ -98,15 +106,82 @@ int main(int argc, char *argv[])
 	ring->cur += 10;
 	fd_ringmarker_mark(end);
 
-	/* Test 1: first level IB check: */
+	printf("Test 1: first level IB check:\n");
 	fd_ringmarker_flush(start);
 	sleep(1);
 
-	/* Test 2: second level IB check: */
+	printf("Test 2: second level IB check:\n");
 	ring->cur = ring->last_start = ring->start;
 	OUT_IB(ring, start, end);
 	fd_ringbuffer_flush(ring);
 	sleep(1);
 
+	printf("Test 3: invalid submit:\n");
+	test_invalid_submit(fd, dev);
+	sleep(1);
+
 	return 0;
+}
+
+static int test_invalid_submit(int fd, struct fd_device *dev)
+{
+	struct fd_bo *bo = fd_bo_new(dev, 0x1000, 0);
+	struct fd_bo *cmd = fd_bo_new(dev, 0x1000, 0);
+	struct drm_msm_gem_submit_bo bos[3] = {
+		[0] = {
+			.handle     = fd_bo_handle(cmd),
+			.flags      = MSM_SUBMIT_BO_READ,
+		},
+		[1] = {
+			.handle     = fd_bo_handle(bo),
+			.flags      = MSM_SUBMIT_BO_READ,
+		},
+		[2] = {
+			/* this is invalid.. there should not be two entries
+			 * for the same bo, instead a single entry w/ all
+			 * usage flags OR'd together should be used.  Kernel
+			 * should catch this, and return an error code after
+			 * cleaning up properly (not leaking any bo's)
+			 */
+			.handle     = fd_bo_handle(bo),
+			.flags      = MSM_SUBMIT_BO_WRITE,
+		},
+	};
+	struct drm_msm_gem_submit_reloc relocs[2] = {
+		[0] = {
+			.submit_offset = 4 * 1,/* cmdbuf[1] */
+			.reloc_idx  = 1,       /* bos[1] */
+		},
+		[1] = {
+			.submit_offset = 4 * 2,/* cmdbuf[2] */
+			.reloc_idx  = 1,       /* bos[2] */
+		},
+	};
+	struct drm_msm_gem_submit_cmd cmds[1] = {
+		[0] = {
+			.type       = MSM_SUBMIT_CMD_BUF,
+			.submit_idx = 0,      /* bos[0] */
+			.size       = 4 * 4,  /* 4 dwords in cmdbuf */
+			.nr_relocs  = ARRAY_SIZE(relocs),
+			.relocs     = VOID2U64(relocs),
+		},
+	};
+	struct drm_msm_gem_submit req = {
+			.pipe       = MSM_PIPE_3D0,
+			.nr_bos     = ARRAY_SIZE(bos),
+			.bos        = VOID2U64(bos),
+			.nr_cmds    = ARRAY_SIZE(cmds),
+			.cmds       = VOID2U64(cmds),
+	};
+	uint32_t *cmdbuf = fd_bo_map(cmd);
+
+	/* CP_NOP packet, payload length 3:
+	 * (use a no-op packet so gpu will ignore)
+	 */
+	cmdbuf[0] = CP_TYPE3_PKT | ((3-1) << 16) | ((CP_NOP & 0xff) << 8);
+	cmdbuf[1] = 0;      /* reloc[0] */
+	cmdbuf[2] = 0;      /* reloc[1] */
+	cmdbuf[3] = 0;      /* unused */
+
+	return drmCommandWriteRead(fd, DRM_MSM_GEM_SUBMIT, &req, sizeof(req));
 }
